7bc3f57159050c041c3fce8bb0d38f05538397b7
ainura-z <aynura_zakirova@mail.ru>
2023-04-18 18:50:58 +0300

    change

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat data/fraudTrain1.csv data/fraudTrain2.csv data/fraudTrain3.csv data/fraudTrain4.csv > data/fraudTrain.csv
+cat data/transactions1.csv data/transactions2.csv > data/transactions.csv
